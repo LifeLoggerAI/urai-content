6762e48c1d2b3ec567fd867ecaf087f44cc74aeb
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-29 12:39:47 -0500

    Add compatibility authorization exports

---
+++ apps/web/src/server/auth/authorization.ts
@@ -0,0 +1,13 @@
+export type { AuthRole, AuthSession, AuthorizationResult } from './roles';
+export { AUTH_ROLES, isKnownAuthRole } from './roles';
+export {
+  canCreateCreatorSubmission,
+  canCreateOwnedResource,
+  canReadOwnedResource,
+  hasPermission,
+  isAdminSession,
+  isCreatorSession,
+  isSignedIn,
+  requireAdmin,
+  requirePermissionResult
+} from './rbac';
